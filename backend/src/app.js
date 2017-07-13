81503d63168f437e67b6c4057df9803474ba9cf8
Marcio Matteucci <marcio.matteucci@gmail.com>
2017-07-13 01:05:07 -0300

    Preparado para prod

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,6 +11,8 @@ const authentication = require('./routes/authentication')(router); // Import Aut
 const blogs = require('./routes/blogs')(router); // Import blog Routes
 const bodyParser = require('body-parser'); // Parse incoming request bodies in a middleware before your handlers, available under the req.body property.
 const cors = require('cors'); // CORS is a node.js package for providing a Connect/Express middleware that can be used to enable CORS with various options.
+const port = process.env.PORT || 8080;
+
 
 // Database Connection
 mongoose.Promise = global.Promise;
@@ -26,18 +28,17 @@ mongoose.connect(config.uri, { useMongoClient: true }, (err) => {
 app.use(cors({ origin: 'http://localhost:4200' }));
 app.use(bodyParser.urlencoded({ extended: false })); // parse application/x-www-form-urlencoded
 app.use(bodyParser.json()); // parse application/json
-//app.use(express.static(__dirname + '/client/dist/')); // Provide static directory for frontend
+app.use(express.static(__dirname + '/public')); // Provide static directory for frontend
 app.use('/authentication', authentication);
 app.use('/blogs', blogs);
 
 // Connect server to Angular 2 Index.html
-/*
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/public/index.html'));
 });
-*/
+
 
 // Start Server: Listen on port 8080
-app.listen(8080, () => {
-  console.log('Escuchando en el Puerto: 8080');
+app.listen(port, () => {
+  console.log('Escuchando en el Puerto: ' + port);
 });
