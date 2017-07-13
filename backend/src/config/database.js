a86e604f9f8b439d3d31e03853fccd0ed08bb5ac
Marcio Matteucci <marcio.matteucci@gmail.com>
2017-07-13 01:18:00 -0300

    mLab db agregada

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -3,7 +3,7 @@ const crypto = require('crypto').randomBytes(256).toString('hex');
 
 // Export config object
 module.exports = {
-  uri: 'mongodb://localhost:27017/mean-blog-db', // Databse URI and database name
+  uri: 'mongodb://marcio:marcio@ds157342.mlab.com:57342/mean-blog', // Databse URI and database name
   secret: crypto, // Cryto-created secret
-  db: 'mean-blog-db' // Database name
+  db: 'mean-blog' // Database name
 }
